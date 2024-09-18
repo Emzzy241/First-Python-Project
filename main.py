83253431bf49548f990ab817380941522e7c3f99
Emzzy241 <emzzyoluwole@gmail.com>
2024-09-18 22:16:21 +0100

    more python learning

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
-myVar = "Hello There"
+# myVar = "Hello There"
 
-print("Hello World")
-print()
-print()
-print("Hey there, an welcome to my python app")
-print()
-print()
-print (myVar)
+# print("Hello World")
+# print()
+# print()
+# print("Hey there, an welcome to my python app")
+# print()
+# print()
+# print (myVar)
+
+# Taking in inputs from users
+
+# print("Enter your first name: ")
+# first_name = input()
+# print()
+# print("Hola " + first_name + " yo soy python")
+
+# using the raised to power operator
+# print(2**4) # when 2 is raised to the power of 4, we have 16
+
+
+# collecting user's first ad lastname
+# print("Enter your firstname: ")
+# first_name = input()
+# print()
+# print("Enter your lastname: ")
+# last_name = input()
+
+# full_name = first_name + " " + last_name
+
+# intro = "my name is " + full_name
+
+# print()
+# print(intro)
+
+
+# name = "jude"
+# print(name + " " * 3)
+
+
+# Bio Data for a user
+print(" Enter your first name: ")
+first_name = input()
+print(" Enter your last name ")
+last_name = input()
+print(" Enter your age: ")
+age = input()
+
+print("Your name is " + first_name + ", your last name is " +  last_name + ", and your are " + age + " years old")
